c4d4d020df2a377081481d6b2233d99acf32708b
robin <robin@robin-Inspiron-5758>
2025-02-10 09:05:02 +0000

    Fix params Plus N (6)

--- src/processes/rulesets.js
+++ src/processes/rulesets.js
@@ -129,7 +129,7 @@ const rulesets = {
 
         let scoreItem13 = {rule: "Params Plus N (6)", ruleNum: 13, skip: false, sequenceNum: 2,
             retain: false, score: 0, completionRound: -1, max: 4, startRoundNum: 28, 
-            n: 3, outBlockStart: 0, outBlockLen: 8, inBlockStart: 0, inBlockLen: 8
+            n: 6, outBlockStart: 0, outBlockLen: 8, inBlockStart: 0, inBlockLen: 8
         };
         this.scoreList.push(scoreItem13);
         this.ruleFunction.push(this.paramsPlusN);
