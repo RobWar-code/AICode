6cec231089c1d4b567d9352e9602f98011d694ad
robin <robin@robin-Inspiron-5758>
2025-05-29 09:47:16 +0100

    Alter config to overcome stdio 6 cpus issue

--- src/AICodeConfig.js
+++ src/AICodeConfig.js
@@ -2,5 +2,5 @@ module.exports = {
     databaseType: "sqlite", // sqlite or mysql
     processMode: "parallel", // parallel or serial
     workerDataTransfer: "stdio", // database or fileSystem or stdio
-    numProcessesSet: "auto" // n or "auto"
+    numProcessesSet: 2 // n or "auto"
 }
