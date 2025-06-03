bb6bd8d0a1b4c267ebf442f7aa563b2638fbd821
robin <robin@robin-Inspiron-5758>
2025-06-03 15:01:19 +0100

    Adjust number of workers to 2

--- src/AICodeConfig.js
+++ src/AICodeConfig.js
@@ -2,5 +2,5 @@ module.exports = {
     databaseType: "sqlite", // sqlite or mysql
     processMode: "parallel", // parallel or serial
     workerDataTransfer: "stdio", // database or fileSystem or stdio
-    numProcessesSet: 3 // n or "auto"
+    numProcessesSet: 2 // n or "auto"
 }
